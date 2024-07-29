03c49ce092d8416745d4ce6bab45e92a0ae70b14
RuanPablo-7 <ToyaMON777@outlook.com>
2024-07-29 00:38:43 -0300

    finalizando

--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 
 import express from "express"
 import { PrismaClient } from '@prisma/client'
+import cors from "cors"
 
 const prisma = new PrismaClient()
 
 
 const app = express()
 app.use(express.json())
-
+app.use(cors())
 
 app.post("/usuarios", async (req, res) => {
     try {
