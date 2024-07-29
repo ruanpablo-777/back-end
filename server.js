ec2bf38861217c4f186008551748af5b62a0efc4
RuanPablo-7 <ToyaMON777@outlook.com>
2024-07-29 19:49:42 -0300

    update

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.get("/", (req, res) => {
+    res.send("hello world")
+})
+
 app.post("/usuarios", async (req, res) => {
     try {
         await prisma.user.create({
